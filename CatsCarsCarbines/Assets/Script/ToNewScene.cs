4555f6fc0362c7ecbfb7bb7a56ea2f4b60ee0f68
Lisa Pham <lvp1731@rit.edu>
2024-05-17 19:36:30 -0400

    create a start scene
    simple start scene with play button to garage scene

---
+++ CatsCarsCarbines/Assets/Script/ToNewScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToNewScene : MonoBehaviour
+{
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
